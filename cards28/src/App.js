d556e83763fdcbec140e10511ac8610089176fc5
Natalia Novikova <106342111+NataliNovi@users.noreply.github.com>
2023-01-08 21:36:03 +0300

    result

--- cards28/src/App.js
+++ cards28/src/App.js
@@ -8,10 +8,10 @@ function App() {
 
   return (
     <div className="App">
-     <Card price = "300" color="1" isActive={isActive==="1"} onButtonClick={setIsActive}/>
-     <Card price = "450" color="2" isActive={isActive==="2"} onButtonClick={setIsActive}/>
-     <Card price = "550" color="3" isActive={isActive==="3"} onButtonClick={setIsActive}/>
-     <Card price = "1000" color="4" isActive={isActive==="4"} onButtonClick={setIsActive}/>
+     <Card price = "300" color="1" colorLimit="1" speed="10" isActive={isActive==="1"} onButtonClick={setIsActive}/>
+     <Card price = "450" color="2" colorLimit="2" speed="50" isActive={isActive==="2"} onButtonClick={setIsActive}/>
+     <Card price = "550" color="3" colorLimit="3" speed="100" isActive={isActive==="3"} onButtonClick={setIsActive}/>
+     <Card price = "1000" color="4" colorLimit="4" speed="200" isActive={isActive==="4"} onButtonClick={setIsActive}/>
     </div>
   );
 }
--- cards28/src/components/Card.jsx
+++ cards28/src/components/Card.jsx
@@ -4,7 +4,7 @@ export default function Card(props) {
 
     console.log(props.price, "drawing");
 
-    const {price, color, isActive, onButtonClick} = props;
+    const {price, color, colorLimit, speed, isActive, onButtonClick} = props;
 
     console.log(props.color);
 
@@ -21,11 +21,16 @@ export default function Card(props) {
 
     //const [isActive, setIsActive] = useState(false); - удаляем, тк нам нужно управлять компонентами сверху, а не сами компоненты чтобы управляли собой.
 
-    let className = "card";
-    className += ' card_' + color;
+    let classNameCard = "card";
+    classNameCard += ' card_' + color;
+
+    let classNameLimit = "limit";
+    classNameLimit += ' limit_' + colorLimit;
+
+
 
     if (isActive) {
-        className += '  card_active';
+        classNameCard += '  card_active';
     }
 
     function onClick(){
@@ -44,11 +49,17 @@ export default function Card(props) {
 
     }
     return (
-    <div className={className}>
-        <header>Безлимитный {price}</header>
-        <div> руб {price} мес</div>
-        <footer>Объём включенного трафика не ограничен</footer>
-        <button onClick={onClick}>{isActive ? "To Basket" : "Buy Now"}</button>
+        <div className='container'>
+    <div onClick={onClick} className={classNameCard}>
+        <header className={classNameLimit}>Безлимитный {price}</header>
+        <div className='money'> <span className="price"> {price} </span> <span className='month'>руб/мес</span></div>
+        <div className='speed'>До {speed} Мбит/сек</div>
+        <footer className='footer'>Объём включенного трафика не ограничен</footer>
+        </div>
     </div>
     )
 }
+
+
+
+//<button onClick={onClick}>{isActive ? "To Basket" : "Buy Now"}</button>
